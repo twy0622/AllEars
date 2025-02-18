4431b990126034d1dd001183bc2d8cd62108ec0e
twy0622 <twy0622@gmail.com>
2025-02-18 17:47:44 +0800

    Tested the endpoints to ensure it generates the correct outputs

--- backend/test.py
+++ backend/test.py
@@ -1,7 +1,7 @@
 """NON-FASTAPI implementation for testing"""
 
 from services.constants import *
-from services.passage_gen import generate_monologue, generate_dialogue
+from services.passage_gen import generate, generate_monologue, generate_dialogue
 from services.audio_gen import synthesize_monologue, synthesize_dialogue
 from config import *
 from services.utils import *
@@ -14,40 +14,79 @@ def generate_test():
         passages = []
         questions = []
         for i in range(NUM_MONOLOGUE):
+            #* Generate
             # data = generate_monologue()
             # with open(f"{TEXT_OUTPUT_DIR}monologue{i}.txt", "w") as f:
             #     f.write(data)
+            
+            #* Read
             with open(f"{TEXT_OUTPUT_DIR}monologue{i}.txt", "r") as f:
                 data = f.read()
+            
             data = json.loads(data, strict=False)
             audio_path = synthesize_monologue(data['text'], data['gender'], f"{AUDIO_OUTPUT_DIR}monologue{i}.wav")
             
             passages.append({
-                "content": data,
-                "audio_url": audio_path
+                "type": "monologue",
+                "topic": data['topic'],
+                "content": data['text'],
+                "audio_url": audio_path,
+                "questions": parse_questions(data, NUM_QUESTION_PER_PASSAGE)
             })
-            
-            questions.append(parse_questions(data, NUM_QUESTION_PER_PASSAGE))
         
         for i in range(NUM_DIALOGUE):
+            #* Generate
             # data = generate_dialogue()
             # with open(f"{TEXT_OUTPUT_DIR}dialogue{i}.txt", "w") as f:
             #     f.write(data)
+            
+            #* Read
             with open(f"{TEXT_OUTPUT_DIR}/dialogue{i}.txt", "r") as f:
                 data = f.read()
+            
             data = json.loads(data, strict=False)
             audio_path = synthesize_dialogue(data['dialogue'], f"{AUDIO_OUTPUT_DIR}dialogue{i}.wav")
             
             passages.append({
-                "content": data,
-                "audio_url": audio_path
+                "type": "dialogue",
+                "topic": data['scenario'],
+                "content": data['dialogue'],
+                "audio_url": audio_path,
+                "questions": parse_questions(data, NUM_QUESTION_PER_PASSAGE)
             })
               
-            questions.append(parse_questions(data, NUM_QUESTION_PER_PASSAGE))
-              
-        return {"passages": passages}
+        return { "passages": passages }
         
     except Exception as e:
-        print(str(e))
+        print(f"Error: {str(e)}")
 
-print(json.dumps(generate_test(), indent=2))
+def grade_subjective(user_answer: str, expected_answer: str, question_text: str, passage_text: str):
+    grading_input = GRADING_PROMPT.format(
+        passage_text=passage_text,
+        question5_text=question_text,
+        expected_answer=expected_answer,
+        user_answer=user_answer
+    )
+    response = generate(grading_input, 512)
+    # grading_result = json.loads(response.text, strict=False)
+    return response
+
+#! generate_test() output
+# print(json.dumps(generate_test(), indent=2))
+
+#! grade_subjective output
+with open(f"{TEXT_OUTPUT_DIR}/dialogue0.txt", "r") as f:
+    data = f.read()
+data = json.loads(data, strict=False)
+
+print(grade_subjective(
+    user_answer="It is more cost-effective, and provides quicker results.",
+    expected_answer=data['question5']['expected_answer'],
+    question_text=data['question5']['question'],
+    passage_text=data['dialogue']
+))
+
+# user_answer="It is more cost-effective, and provides quicker results."
+# expected_answer=data['question5']['expected_answer']
+# question_text=data['question5']['question']
+# passage_text=data['dialogue']
